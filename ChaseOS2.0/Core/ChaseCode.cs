78d359e550c17013bf66c29468fab98497e8f9bf
ChaseOSDev <devChase0842@outlook.com>
2021-09-29 20:21:27 -0400

    rainbow.exe

--- ChaseOS2.0/Core/ChaseCode.cs
+++ ChaseOS2.0/Core/ChaseCode.cs
@@ -62,10 +62,81 @@ namespace ChaseOS2._0.Core
         }
         public void Command(string[] segment)
         {
-            
+
 
             switch (segment[1])
             {
+                case "rainbow":
+                    if (Commands.adminScript)
+                    {
+
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Magenta;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Magenta;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.Clear();
+                    
+                    }
+                    break;
                 case "shutdown":
                     if (Commands.adminScript)
                     {
